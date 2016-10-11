5cc72fefe3e7b813c7f0d56e243a2507c7cd044d
Dennis Drost <dennisrdrost@gmail.com>
2016-10-11 08:34:42 +0200

    Added a theory to the tests as example and to see if everything works

---
+++ DataModel.Tests/BasicTest.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace DataModel.Tests
+{
+    public class BasicTest
+    {
+        [Fact]
+        public void TestAreRunningCorrect()
+        {
+            var correct = 4;
+            correct.Should().Be(4);
+        }
+
+        [Theory]
+        [InlineData(2, 3, 6)]
+        [InlineData(3, 2, 6)]
+        [InlineData(5, 6, 30)]
+        public void BasicTheorysAreRunningCorrect(int value1, int value2, int shouldBe)
+        {
+            var answer = value1*value2;
+
+            answer.Should().Be(shouldBe);
+        }
+
+    }
+}
